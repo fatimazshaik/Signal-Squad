aa26525e2d4f2b9105d630f750e462167b7939d2
Fatima Shaik <fzshaik@ucdavis.edu>
2025-03-11 09:52:30 -0700

    debugging action segmentation w merged code

--- all_algor_merge_IOU_Path.py
+++ all_algor_merge_IOU_Path.py
@@ -102,14 +102,14 @@ def is_person_lying_down(left_shoulder, right_shoulder, left_hip, right_hip, lef
 
     # Handle cases where all x-values are the same (vertical body)
     if np.all(key_x == key_x[0]):
-        print("Warning: Vertical body detected, skipping linear regression")
+        # print("Warning: Vertical body detected, skipping linear regression")
         is_horizontal = False
     else:
         try:
             m, _ = np.polyfit(key_x, key_y, 1)  # Fit y = mx + b
             is_horizontal = abs(m) < 0.3  # Threshold for horizontal alignment
         except np.linalg.LinAlgError:
-            print("Warning: np.polyfit failed due to singular matrix")
+            # print("Warning: np.polyfit failed due to singular matrix")
             is_horizontal = False
 
     # **Keypoint Distance Check**
@@ -132,22 +132,30 @@ def is_person_lying_down(left_shoulder, right_shoulder, left_hip, right_hip, lef
 # number = 7
 
 # GLOBAL VARS
-object_csv_file = "/home/jsguo/EEC174/Signal-Squad/table_csv/object_data_table1.csv"
-pose_csv_file = "/home/jsguo/EEC174/Signal-Squad/table_csv/pose_data_table1.csv"
+object_csv_file = "object_data.csv"
+pose_csv_file = "pose_data.csv"
 count_intersection = 0
-interest_frames= []
+table_true = 0
+new_action = 0
+action_type_all = []
+interest_frames_all = []
 action_type = []
+interest_frames= []
 ious = []
+laying_frame_all = []
+up_frame_all = []
 no_person = True
 person_index = 0
 object_index = 0
+couch_flag = 0
 
 # VARS for Path
-csv_file = "/home/jsguo/EEC174/Signal-Squad/output_vector_path/bed1-person-tracking.csv"
+csv_file = "tracking.csv" # CHANGE
 threshold_fridge = 700  # Threshold for detecting a sitting down or standing up event (in pixels)
 threshold_chair = 100
 threshold_couch = 50
 threshold_bed = 150
+action_events_all = []
 action_events = []
 
 # Initialize lists to store y_positions and frame indexes
@@ -180,7 +188,6 @@ df_pose = pd.read_csv(pose_csv_file, header=None, names=['Frame', 'Nose', 'Left 
 # Get max frame
 max_frame_count = max(df_object.Frame)
 frames = df_pose.Frame
-print(max_frame_count)
 
 # Iterate through all frames:
 for i in range(1, max_frame_count+1):
@@ -199,6 +206,7 @@ for i in range(1, max_frame_count+1):
             no_person = True
         
         if(not no_person):
+            new_action = 0
             x_center_bb1 = df_object.loc[row_indices[0], 'x_center']
             x_center_bb2 = df_object.loc[row_indices[1], 'x_center']
 
@@ -253,13 +261,14 @@ for i in range(1, max_frame_count+1):
                     previous_y_up = x_positions[i - 20]
                     frame_idx = frame_indexes[i]
 
-                    print(i, current_y, previous_y)
+                    # print(i, current_y, previous_y)
 
                     # Detect "sitting down" (laying down): Significant decrease in y_position
                     if (current_y < previous_y - threshold_fridge) and up_frame == 0:
                         action_events.append(('Sitting Down', frame_idx, current_y))
                         if (frame_idx - laying_frame > 10):
                             laying_frame = frame_idx
+
                     
                     # Detect "standing up": Significant increase in y_position
                     elif current_y > previous_y_up + threshold_fridge and laying_frame != 0:
@@ -291,22 +300,26 @@ for i in range(1, max_frame_count+1):
                             action_events.append(('Standing Up', frame_idx, current_y))
                             #if (frame_idx - up_frame > 20):
                             up_frame = frame_idx
-
-
-            # if(iou>=0.3 and  (df_object.loc[row_indices[object_index], 'Object'] == "chair")): #Check if Sitting on Chair Besides Table
-            #     interest_frames.append(df_object.loc[row_indices[person_index], 'Frame'])
-            #     action_type.append("Sitting on Table")
-            #     count_intersection += 1
-            # elif(iou>=0.45) and (df_object.loc[row_indices[object_index], 'Object']) == "refrigerator": #Check if Opening Fridge
-            #     interest_frames.append(df_object.loc[row_indices[person_index], 'Frame'])
-            #     action_type.append("Opening Fridge")
-            #     count_intersection += 1
-            # elif((iou>=0.4) and (df_object.loc[row_indices[object_index], 'Object'] == "couch")): #Check if Sitting On Couch
-            #     interest_frames.append(df_object.loc[row_indices[person_index], 'Frame'])
-            #     action_type.append("Sitting on Couch")
-            #     count_intersection += 1
-
+    
+    elif (action_type != []) and (i in df_object.index): 
+        if(len(df_object[df_object.Frame==i])==1) and (df_object.loc[i,"Object"]!= "person"):
+            if new_action == 30:#og 50
+                couch_flag = 0
+                action_events_all.append(action_events)
+                action_events = []
+                action_type_all.append(action_type)
+                action_type = []
+                interest_frames_all.append(interest_frames)
+                interest_frames = []
+                laying_frame_all.append(laying_frame)
+                laying_frame = 0
+                up_frame_all.append(up_frame)
+                up_frame = 0
+                new_action = 0
+            else:
+                new_action += 1
     else:
+        new_action = 0
         row_index = df_pose.query("Frame == " + str(i)).index.tolist()
         if(row_index != []):
             right_shoulder = eval(df_pose.iloc[row_index]["Right Shoulder"].tolist()[0])
@@ -330,48 +343,90 @@ for i in range(1, max_frame_count+1):
                 count_intersection += 1
                 action_type.append("Laying on A Bed")
 
-            if (i > 30 and i < len(y_positions)):
-                current_y = y_positions[i]
-                previous_y = y_positions[i - 20]
-                previous_y_up = y_positions[i - 20]
-                frame_idx = frame_indexes[i]
+        if (i > 30 and i < len(y_positions)):
+            current_y = y_positions[i]
+            previous_y = y_positions[i - 20]
+            previous_y_up = y_positions[i - 20]
+            frame_idx = frame_indexes[i]
                 
                 # Detect "sitting down" (laying down): Significant decrease in y_position
-                if (current_y > previous_y + threshold_bed) and up_frame == 0:
-                    action_events.append(('Sitting Down', frame_idx, current_y))
-                    if (frame_idx - laying_frame > 10):
-                        laying_frame = frame_idx
+            if (current_y > previous_y + threshold_bed) and up_frame == 0:
+                action_events.append(('Sitting Down', frame_idx, current_y))
+                if (frame_idx - laying_frame > 10):
+                    laying_frame = frame_idx
                 
-                # Detect "standing up": Significant increase in y_position
-                elif current_y < previous_y_up - threshold_bed and laying_frame != 0:
-                    action_events.append(('Standing Up', frame_idx, current_y))
-                    #if (frame_idx - up_frame > 20):
-                    up_frame = frame_idx
+            # Detect "standing up": Significant increase in y_position
+            elif current_y < previous_y_up - threshold_bed and laying_frame != 0:
+                action_events.append(('Standing Up', frame_idx, current_y))
+                #if (frame_idx - up_frame > 20):
+                up_frame = frame_idx
 
+    if((i==max_frame_count) and (action_type != [])): # edge case
+        action_events_all.append(action_events)
+        action_events = []
+        action_type_all.append(action_type)
+        action_type = []
+        interest_frames_all.append(interest_frames)
+        interest_frames = []
+        laying_frame_all.append(laying_frame)
+        laying_frame = 0
+        up_frame_all.append(up_frame)
+        up_frame = 0
 # print(action_type)
 # Get max action_type
-target_frames = []
-mode = statistics.mode(action_type)
-print("Action Type: ", mode)
-mode_list = [i for i, val in enumerate(action_type) if val == mode]
-for x in mode_list:
-    target_frames = interest_frames[x]
-
-# Get associated max and min frames
-max_frame = max(interest_frames)
-min_frame = min(interest_frames)
-start_time = min_frame
-end_time = max_frame
 
 
-# Print associated start/end time
-print("IOU")
-print("Start Time ", start_time)
-print("End Time ", end_time)
-# Print duration
-print("Duration: ", end_time-start_time)
+print("Number of Actions: ", len(action_type_all))
+for i in range(len(action_type_all)):
+    laying_frame = laying_frame_all[i] if i <= (len(laying_frame_all) - 1) else 0
+    up_frame = up_frame_all[i] if i <= (len(up_frame_all) - 1) else 0
+    event = action_events_all[i] if i <= (len(up_frame_all) - 1) else 0
+    action = action_type_all[i]
+    target_frames = interest_frames_all[i]
+    mode = statistics.mode(action)
+    mode1 = statistics.mode(event) if event != [] else 0
+    print(mode1)
+    print(mode)
+    # if(action[0] == 'Sitting on Couch' and action[len(action)-1] == 'Sitting on Couch'):
+    #     mode = 'Sitting on Couch'
+    # # Get associated max and min frames
+    # max_frame = max(target_frames)
+    # min_frame = min(target_frames)
+    # start_time = min_frame/30
+    # end_time = max_frame/30
+    # if (abs(start_time - up_frame) < 30):
+    #     merge_start_time = (start_time + up_frame)/2
+    # else:
+    #     if (mode == "Laying on A Bed"):
+    #         merge_start_time = up_frame
+    #     else: 
+    #         merge_start_time = start_time
 
-print("Path")
-print("up_frame", up_frame)
-print("laying_frame", laying_frame)
-print("Difference", laying_frame-up_frame)
+    # if (abs(end_time - laying_frame) < 30):
+    #     merge_end_time = (end_time + laying_frame)/2
+    # else:
+    #     if (mode == "Laying on A Bed"):
+    #         merge_end_time = laying_frame
+    #     else: 
+    #         merge_end_time = end_time
+
+    # # Print associated start/end time
+    # print()
+    # print("Action Type: ", mode)
+    # print()
+    # print("IOU")
+    # print("Start Time IOU", start_time)
+    # print("End Time IOU", end_time)
+    # print("Duration: ", end_time-start_time)
+    # print()
+
+    # print("Path")
+    # print("Start Time Path", up_frame)
+    # print("End Time Path", laying_frame)
+    # print("Difference", laying_frame-up_frame)
+    # print()
+
+    # print("MERGE TIME")
+    # print("Start Time Path", merge_start_time)
+    # print("End Time Path", merge_end_time)
+    # print("Difference", merge_end_time-merge_start_time)
